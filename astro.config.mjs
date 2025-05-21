26d805e029be3a78433ba2e874a7aeea3be5bf95
brotundu <124742385+brotundusol@users.noreply.github.com>
2025-05-21 11:48:14 +0300

    Refreshed CV page and added a new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://brotundu.solutions',
+  site: 'https://brotundu.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
